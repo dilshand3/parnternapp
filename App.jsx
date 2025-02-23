82d27a7ab04f25ae91942bd681cb0874cdb305a0
dilshan.d3 <dilshan72404@gmail.com>
2025-02-23 10:19:51 +0530

    icon added in

--- App.jsx
+++ App.jsx
@@ -1,15 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-      <Text>i am dilshan</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>App</Text>
+      <Text style={styles.text}>I am Dilshan</Text>
+      <Text style={styles.text}>I am the new world</Text>
+      <AntDesign name="eye" color="green" size={30} />
     </View>
-  )
-}
+  );
+};
 
 export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+});
